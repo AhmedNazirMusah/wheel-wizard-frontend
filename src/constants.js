77494dca70c95062e5257fcd1152a6c7564d25db
ahmednazirmusah <ahmed999nazir@gmail.com>
2026-04-25 15:24:40 -0700

    Point frontend to Render API service

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const URL = process.env.REACT_APP_API_URL || 'https://wheel-wizard.onrender.com';
+export const URL = process.env.REACT_APP_API_URL || 'https://wheel-wizard-api.onrender.com';
 export const DB = 'POSTGRESQL';
